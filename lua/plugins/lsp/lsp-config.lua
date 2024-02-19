1a04472269cd2a532c362f0c89b9761bdc675b51
arturfil <arturfil@hotmail.com>
2024-02-18 21:14:17 -0500

    works as expected

--- lua/plugins/lsp/lsp-config.lua
+++ lua/plugins/lsp/lsp-config.lua
@@ -19,46 +19,60 @@ return {
 			opts.buffer = bufnr
 
 			-- set keybinds
-			opts.desc = "Show LSP references"
-			keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
+			-- opts.desc = "Show LSP references"
+			-- keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
 
-			opts.desc = "Go to declaration"
-			keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
+			-- opts.desc = "Go to declaration"
+			-- keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
 
-			opts.desc = "Show LSP definitions"
-			keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
+			-- opts.desc = "Show LSP definitions"
+			-- keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
 
-			opts.desc = "Show LSP implementations"
-			keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
+			-- opts.desc = "Show LSP implementations"
+			-- keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
 
-			opts.desc = "Show LSP type definitions"
-			keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
+			-- opts.desc = "Show LSP type definitions"
+			-- keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
 
-			opts.desc = "See available code actions"
-			keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
+			-- opts.desc = "See available code actions"
+			-- keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
 
-			opts.desc = "Smart rename"
-			keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
+			-- opts.desc = "Smart rename"
+			-- keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
 
-			keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+			-- keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
 
-			opts.desc = "Show buffer diagnostics"
-			keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
+			-- opts.desc = "Show buffer diagnostics"
+			-- keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
 
-			opts.desc = "Show line diagnostics"
-			keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
+			-- opts.desc = "Show line diagnostics"
+			-- keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
 
-			opts.desc = "Go to previous diagnostic"
-			keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
+			-- opts.desc = "Go to previous diagnostic"
+			-- keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
 
-			opts.desc = "Go to next diagnostic"
-			keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
+			-- opts.desc = "Go to next diagnostic"
+			-- keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
 
-			opts.desc = "Show documentation for what is under cursor"
-			keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
+			-- opts.desc = "Show documentation for what is under cursor"
+			-- keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
 
-			opts.desc = "Restart LSP"
-			keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
+			-- opts.desc = "Restart LSP"
+			-- keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
+
+            keymap.set("n", "gf", "<cmd>Lspsaga finder<CR>", opts)
+            keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+            keymap.set("n", "gd", "<cmd>Lspsaga peek_definition<CR>", opts)
+            keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+            keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts)
+            keymap.set("n", "<leader>rn", "<cmd>Lspsaga rename<CR>", opts)
+            keymap.set("n", "<leader>d", "<cmd>Lspsaga show_line_diagnostics<CR>", opts)
+            keymap.set("n", "<leader>d", "<cmd>Lspsaga show_cursor_diagnostics<CR>", opts)
+            keymap.set("n", "[d", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts)
+            keymap.set("n", "]d", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts)
+            keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts)
+            keymap.set("n", "<leader>o", "<cmd>LSoutlineToggle<CR>", opts)
+
 		end
 
 		-- used to enable autocompletion (assign to every lsp server config)
