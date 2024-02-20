5817352faad150a7d27b6c49eee75e3b50e007cc
arturfil <arturfil@hotmail.com>
2024-02-20 03:11:30 -0500

    changed keybindings

--- lua/plugins/lsp/none-ls.lua
+++ lua/plugins/lsp/none-ls.lua
@@ -30,6 +30,8 @@ return {
     -- to setup format on save
     local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
+    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+
     -- configure null_ls
     null_ls.setup({
       -- add package.json as identifier for root (for typescript monorepos)
