ec00a6ff48bf63316834a52ff03eeb4bdfa32c12
arturfil <arturfil@hotmail.com>
2024-02-19 19:32:41 -0500

    added python debugger

--- lua/plugins/debuggin.lua
+++ lua/plugins/debuggin.lua
@@ -1,30 +1,48 @@
 return {
-    "mfussenegger/nvim-dap",
-    dependencies = {
-        "rcarriga/nvim-dap-ui",
-        "leoluz/nvim-dap-go"
+    {
+        "mfussenegger/nvim-dap",
+        dependencies = {
+            "rcarriga/nvim-dap-ui",
+            "leoluz/nvim-dap-go"
+        },
+        config = function()
+            local dap, dapui = require("dap"), require("dapui")
+
+            require("dapui").setup()
+            require("dap-go").setup()
+
+
+            dap.listeners.before.attach.dapui_config = function()
+                dapui.open()
+            end
+            dap.listeners.before.launch.dapui_config = function()
+                dapui.open()
+            end
+            dap.listeners.before.event_terminated.dapui_config = function()
+                dapui.close()
+            end
+            dap.listeners.before.event_exited.dapui_config = function()
+                dapui.close()
+            end
+
+            vim.keymap.set("n", "<Leader>dt", ":DapUiToggle<CR>", {})
+            vim.keymap.set("n", "<Leader>db", dap.toggle_breakpoint, {})
+            vim.keymap.set("n", "<Leader>dc", dap.continue, {})
+            vim.keymap.set("n", "<Leader>dr", ":lua require('dapui').open({reset = true})<CR>", {})
+
+            vim.fn.sign_define("DapBreakpoint", {text = "⏺", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl="DapBreakpoint"})
+        end,
     },
-    config = function()
-        local dap, dapui = require("dap"), require("dapui")
-
-        require("dapui").setup()
-        require("dap-go").setup()
-
-
-        dap.listeners.before.attach.dapui_config = function()
-            dapui.open()
-        end
-        dap.listeners.before.launch.dapui_config = function()
-            dapui.open()
-        end
-        dap.listeners.before.event_terminated.dapui_config = function()
-            dapui.close()
-        end
-        dap.listeners.before.event_exited.dapui_config = function()
-            dapui.close()
-        end
-
-        vim.keymap.set("n", "<Leader>dt", dap.toggle_breakpoint, {})
-        vim.keymap.set("n", "<Leader>dc", dap.continue, {})
-    end,
+    {
+        "mfussenegger/nvim-dap-python",
+        ft = "python",
+        dependencies = {
+            "mfussenegger/nvim-dap",
+        },
+        config = function(_, opts)
+            local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python3"
+            require("dap-python").setup(path)
+            -- require("core.utils").load_mappings("dap_python")
+        end,
+    },
 }
--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -51,6 +51,7 @@ return {
         "stylua", -- lua formatter
         "isort", -- python formatter
         "black", -- python formatter
+        "debugpy", -- python debugger
         "golines", -- go formatter
         "pylint", -- python linter
         "eslint_d", -- js linter
