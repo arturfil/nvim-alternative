005bed2fa633b24fe7033b1a29099ad634d2c395
arturfil <arturfil@hotmail.com>
2024-02-27 17:16:59 -0500

    changed telescope file

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,30 +1,14 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  branch = "0.1.x",
+  -- "nvim-telescope/telescope.nvim", tag = "0.1.4",
+  "nvim-telescope/telescope.nvim", branch = "0.1.x",
   dependencies = {
-    "nvim-lua/plenary.nvim",
-    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    'nvim-lua/plenary.nvim',
     "nvim-tree/nvim-web-devicons",
   },
   config = function()
     local telescope = require("telescope")
     local actions = require("telescope.actions")
     local builtin = require('telescope.builtin')
-
-    telescope.setup({
-      defaults = {
-        path_display = { "truncate " },
-        mappings = {
-          i = {
-            ["<C-k>"] = actions.move_selection_previous, -- move to prev result
-            ["<C-j>"] = actions.move_selection_next, -- move to next result
-            ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-          },
-        },
-      },
-    })
-
-    telescope.load_extension("fzf")
 
     -- set keymaps
     local keymap = vim.keymap -- for conciseness
