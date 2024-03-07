e31407be4ff6c2b9f2dfa138e2f1439e8ac10ba4
arturfil <arturfil@hotmail.com>
2024-03-07 12:36:08 -0500

    telescope doesn't crash anymore

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,20 +1,17 @@
 return {
-  -- "nvim-telescope/telescope.nvim", tag = "0.1.4",
   "nvim-telescope/telescope.nvim", branch = "0.1.x",
   dependencies = {
-    'nvim-lua/plenary.nvim',
+    "nvim-lua/plenary.nvim",
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     "nvim-tree/nvim-web-devicons",
   },
-  config = function()
+    config = function()
     local telescope = require("telescope")
-    local actions = require("telescope.actions")
-    local builtin = require('telescope.builtin')
 
     -- set keymaps
-    local keymap = vim.keymap -- for conciseness
-     vim.keymap.set('n', '<c-p>', builtin.find_files, {})
-     vim.keymap.set('n', '<Space><Space>', builtin.oldfiles, {})
-     vim.keymap.set('n', '<Space>fg', builtin.live_grep, {})
-     vim.keymap.set('n', '<Space>fh', builtin.help_tags, {})
+     vim.keymap.set('n', '<C-p>', "<cmd>Telescope find_files<cr>", {})
+     vim.keymap.set('n', '<Space><Space>', "<cmd>Telescope old_files<cr>", {})
+     vim.keymap.set('n', '<Space>fg', "<cmd>Telescope live_grep<cr>", {})
+     vim.keymap.set('n', '<Space>fh', "<cmd>Telescope help_tags<cr>", {})
   end,
 }
