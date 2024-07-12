2bcbf339e4eba583ba525878039641cd3b956583
arturfil <arturfil@hotmail.com>
2024-07-12 02:26:18 -0400

    format only with keybindings

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -32,12 +32,12 @@ return {
 			},
 		})
 
-		vim.keymap.set({ "n", "v" }, "<leader>mp", function()
-			conform.format({
-				lsp_fallback = true,
-				async = false,
-				timeout_ms = 1000,
-			})
-		end, { desc = "Format file or range (in visual mode)" })
+		-- vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+		-- 	conform.format({
+		-- 		lsp_fallback = true,
+		-- 		async = false,
+		-- 		timeout_ms = 1000,
+		-- 	})
+		-- end, { desc = "Format file or range (in visual mode)" })
 	end,
 }
