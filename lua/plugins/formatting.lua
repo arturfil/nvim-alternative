05ff08f606e6b8e30d34289bbfdba66e4fbab3e4
arturfil <arturfil@hotmail.com>
2024-07-14 02:56:57 -0400

    added new keybinding for formatting

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -25,19 +25,19 @@ return {
 				lua = { "stylua" },
 				python = { "isort", "black" },
 			},
-			format_on_save = {
+			-- format_on_save = {
+			-- 	lsp_fallback = true,
+			-- 	async = false,
+			-- 	timeout_ms = 1000,
+			-- },
+		})
+
+		vim.keymap.set({ "n", "v" }, "<leader>ff", function()
+			conform.format({
 				lsp_fallback = true,
 				async = false,
 				timeout_ms = 1000,
-			},
-		})
-
-		-- vim.keymap.set({ "n", "v" }, "<leader>mp", function()
-		-- 	conform.format({
-		-- 		lsp_fallback = true,
-		-- 		async = false,
-		-- 		timeout_ms = 1000,
-		-- 	})
-		-- end, { desc = "Format file or range (in visual mode)" })
+			})
+		end, { desc = "Format file or range (in visual mode)" })
 	end,
 }
